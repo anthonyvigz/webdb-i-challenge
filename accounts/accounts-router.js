d963618dbd356b5e3ae8f25e01304d4d9ec867b8
anthonyvigz <anthonyvigz@gmail.com>
2019-08-21 19:18:39 -0400

    created get for accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -2,5 +2,16 @@ const router = require('express').Router();
 
 const db = require('../data/dbConfig.js');
 
+router.get('/', (req, res) => {
+    db('accounts')
+    .then((accounts) => {
+      res.json(accounts)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Failed to get users.' })
+    })
+})
+
+
 
 module.exports = router;
