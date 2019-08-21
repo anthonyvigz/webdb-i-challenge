6e90fe9ea2d2e58240b898d5c0f9ca26d29e12bd
anthonyvigz <anthonyvigz@gmail.com>
2019-08-21 19:18:59 -0400

    created get for specific accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -12,6 +12,21 @@ router.get('/', (req, res) => {
     })
 })
 
-
+router.get('/:id', (req, res) => {
+  
+    const { id } = req.params;
+  
+    db('accounts').where({ id })
+    .then((account) => {
+      if (account) {
+        res.json(account)
+      } else {
+        res.status(404).json({ message: 'Could not find account with given ID.' })
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Failed to find account.' })
+    })
+})
 
 module.exports = router;
